95931df5536c5db5eed78fa138042d6559ebe3b4
Benjamin <bdunisch@googlemail.com>
2017-05-01 17:05:00 +0200

    Update Abgabe Level3.py

--- Abgabe Level3.py
+++ Abgabe Level3.py
@@ -10,8 +10,14 @@ answers_easy = ["9", "outfield", "pitcher","catcher" ]
 answers_medium = ["9", "3", "homerun", "flyball"]
 answers_hard = ["max", "homerun", "out", "shortstop"]
 
-def word_in_pos(word, parts_of_speech): 
-    # Check is the part is a part of blanks
+def word_in_pos(word, parts_of_speech):
+    '''Word_in_pos:
+        word: The word that gets check.
+        parts_of_speech: Part of the string where we searcheing
+    Behavior:
+        Finds out the position on the search word
+    Returns:
+        Returns if the Word in there or not'''
     for pos in parts_of_speech:
         # goint to the elements of the list
         if pos in word:
@@ -19,34 +25,53 @@ def word_in_pos(word, parts_of_speech):
             # returns the element if found
     return None
     #returns None if the element is not found
-                   
-def gameplay(final_sentence,sentence,answer,blanks): #Game logging
-    sentence = sentence.split()# into a list
-    # print sentence
-    counter = 0 # for fail trys
-    answerscounter = -1 # for the number in answer
-    for i in sentence:
-        replacement = word_in_pos(i,blanks)
+
+def answer_check(user_input,answer,word,counter_of_fails,answers_counter,replacement):
+    # Check if the answer is correct and if, then but it in the Liste, easy try again (3 Try posible)
+    if user_input == answer[answers_counter]:
+        word = word.replace(replacement,user_input)
+        counter_of_fails = 0 #if the user_input is correct wie will add the answer to the list
+        print "Correct, -->" + user_input
+        return user_input,answer,word,counter_of_fails,answers_counter,replacement
+    while user_input != answer[answers_counter] and counter_of_fails < 3: # if answer is not correct
+        user_input = raw_input("Not correct, try again:")
+        counter_of_fails +=1
+        if counter_of_fails == 3: # after 3 time with the wrong answer
+            print "Anwers where not correct, please try ist later again"
+            user_input = "WRONG ANSWER"
+            replacement = "WRONG ANSWER"
+            return user_input,answer,word,counter_of_fails,answers_counter,replacement
+            break
+
+def gameplay(final_sentence,sentence,answer,blanks): #Game logging,
+    sentence = sentence.split()# String gets splited into a array
+    counter_of_fails = 0 # for fail trys, when the user answer the question
+    answers_counter = -1 # for the number in answer
+    for word in sentence:
+        replacement = word_in_pos(word,blanks)
         if replacement != None: # if there is flied to fill out something
             user_input = raw_input("Your answer for " + replacement + ":")
-            answerscounter += 1 # find the correct position in the answer list
-            if user_input == answer[answerscounter]:
-                i = i.replace(replacement,user_input)
-                counter = 0
+            answers_counter += 1 # find the correct position in the answer list
+            answer_check(user_input,answer,word,counter_of_fails,answers_counter,replacement)
+            '''if user_input == answer[answers_counter]:
+                word = word.replace(replacement,user_input)
+                counter_of_fails = 0 #if the user_input is correct wie will add the answer to the list
                 print "Correct, -->" + user_input
-            while user_input != answer[answerscounter] and counter < 3: # if answer is not correct
+            while user_input != answer[answers_counter] and counter_of_fails < 3: # if answer is not correct
                 user_input = raw_input("Not correct, try again:")
-                counter +=1
-            if counter == 3: # after 3 time with the wrong answer
-                print "Anwers where not correct, please find below the solution  "
-                print answer # showing solutions
-                break
-            i = i.replace(replacement,user_input)
-            final_sentence.append(i)
+                counter_of_fails +=1
+            if counter_of_fails == 3: # after 3 time with the wrong answer
+                print "Anwers where not correct, please try ist later again"
+                break '''
+            word = word.replace(replacement,user_input)
+            final_sentence.append(word)
         else:
-            final_sentence.append(i)
+            final_sentence.append(word)
     final_sentence = " ".join(final_sentence)
-    return final_sentence # pint out list
+    if counter_of_fails == 0:
+        return final_sentence # print out list
+    else:
+        print "try again"
 
 print "" # just that it looks nicer
 print "Welcome to the game where you learn some Baseball rules" #Welcome text
